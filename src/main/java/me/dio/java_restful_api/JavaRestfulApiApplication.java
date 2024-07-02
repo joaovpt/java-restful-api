3dac1c563483952db4d7d963f852a78e8b348769
joaovvpp <joaotassi18@gmail.com>
2024-07-02 09:46:42 -0300

    Initial commit

---
+++ src/main/java/me/dio/java_restful_api/JavaRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.java_restful_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestfulApiApplication.class, args);
+	}
+
+}
